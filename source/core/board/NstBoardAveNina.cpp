7c3536f696f0d4337f29a5966e34369cf815df19
Ryan Danbrook <rdanbrook98@gmail.com>
2013-01-10 13:05:42 -0500

    Fix for mapper 146

--- source/core/board/NstBoardAveNina.cpp
+++ source/core/board/NstBoardAveNina.cpp
@@ -49,10 +49,23 @@ namespace Nes
 
 				void Nina06::SubReset(const bool hard)
 				{
-					// FCEUltra
-					Map( 0x4020U, 0x5FFFU, &Nina06::Poke_4100 );
-					Map( 0x8000U, 0xFFFFU, &Nina06::Poke_4100 );
+					switch (board.GetId())
+					{
+						case Type::AVE_NINA06:
+						
+							Map( 0x4020U, 0x5FFFU, &Nina06::Poke_4100 );
+							Map( 0x8000U, 0xFFFFU, &Nina06::Poke_4100 );
 
+							break;
+
+						default:
+						
+							for (uint i=0x4100; i < 0x6000; i += 0x200)
+								Map( i+0x00, i+0xFF, &Nina06::Poke_4100 );
+
+							break;
+					}
+					
 					if (hard)
 						prg.SwapBank<SIZE_32K,0x0000>(0);
 				}
