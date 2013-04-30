11069a1e1ba445df0ea53484a37da60cf3b1682f
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2013-05-01 00:05:21 +0200

    Update extensions

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -143,7 +143,7 @@ void retro_get_system_info(struct retro_system_info *info)
    info->library_name     = "Nestopia";
    info->library_version  = "v" NST_VERSION;
    info->need_fullpath    = false;
-   info->valid_extensions = "nes|NES|zip|ZIP|fds|FDS"; // Anything is fine, we don't care.
+   info->valid_extensions = "nes|fds";
 }
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
