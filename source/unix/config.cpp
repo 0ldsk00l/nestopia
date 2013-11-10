d48cd41c40b42760b9d55f3744010b8355712183
Ryan Danbrook <rdanbrook98@gmail.com>
2013-11-09 21:27:12 -0500

    Changed types in config structs, reverted last commit (unix)

--- source/unix/config.cpp
+++ source/unix/config.cpp
@@ -23,6 +23,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <glib.h>
+
 #include "config.h"
 
 settings *conf;
--- source/unix/input.cpp
+++ source/unix/input.cpp
@@ -23,6 +23,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <glib.h>
+
 #include "main.h"
 #include "config.h"
 #include "video.h"
