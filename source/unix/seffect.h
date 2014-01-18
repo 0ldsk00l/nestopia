70188aa6198b6d6f31a3a113a7ae4297fce35f0d
Ryan Danbrook <rdanbrook98@gmail.com>
2014-01-18 12:09:29 -0500

    Removed "sound effect" code (unix)

--- source/unix/seffect.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SEFFECT_H_
-#define _SEFFECT_H_
-
-void seffect_init();
-int seffect_surround_lite_process(short *data, int length);
-void seffect_ex_process(long *pBuf,long samples);
-
-#endif
