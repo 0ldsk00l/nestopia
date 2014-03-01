649d6899d064343c1569a5e043948c985b7686aa
Ryan Danbrook <rdanbrook98@gmail.com>
2014-03-01 14:30:16 -0500

    More pruning and cleanups

--- source/unix/fileio.h
+++ source/unix/fileio.h
@@ -1,22 +1,9 @@
-/*
-	NEStopia / Linux
-	Port by R. Belmont
-	
-	fileio.h - movie and state I/O definitions
-*/
-
-#ifndef AUXIO_H
-#define AUXIO_H
+#ifndef FILEIO_H
+#define FILEIO_H
 
 void fileio_init(void);
-void fileio_do_state_save(void);
-void fileio_do_state_load(void);
-void fileio_do_movie_save(void);
-void fileio_do_movie_load(void);
-void fileio_do_movie_stop(void);
 void fileio_set_fds_bios(void);
 void fileio_shutdown(void);
-int fileio_load_archive(const char *filename, unsigned char **dataout, int *datasize, int *dataoffset, const char *filetoload = NULL, char *outname = NULL);
 void fileio_load_db(void);
 
 #endif
