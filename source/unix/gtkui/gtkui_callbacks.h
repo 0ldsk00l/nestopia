696df05ec7ad81a199e1e74234a40e5f77c3141b
Ryan Danbrook <rdanbrook98@gmail.com>
2014-03-31 07:47:46 -0400

    Refactoring

--- source/unix/gtkui/gtkui_callbacks.h
+++ source/unix/gtkui/gtkui_callbacks.h
@@ -2,5 +2,7 @@
 #define _GTKUI_CALLBACKS_H_
 
 void gtkui_cb_reset(GtkWidget *reset, int hard);
+unsigned int gtkui_cb_translate_gdk_sdl(int gdk_keyval);
+int gtkui_cb_convert_key(GtkWidget *grab, GdkEventKey *event, gpointer user_data);
 
 #endif
