495740c9b0a6f7cdf2739c1b8e76bf689e72ae7d
Ryan Danbrook <rdanbrook98@gmail.com>
2014-04-26 14:49:45 -0400

    Initial cheat support

--- source/unix/cheats.h
+++ source/unix/cheats.h
@@ -1,4 +1,14 @@
 #ifndef _CHEATS_H_
 #define _CHEATS_H_
 
+#include "core/api/NstApiEmulator.hpp"
+#include "core/api/NstApiCheats.hpp"
+#include "core/NstStream.hpp"
+#include "core/NstXml.hpp"
+
+using namespace Nes::Api;
+
+void cheats_init();
+void cheats_code_gg_add(const wchar_t *data);
+
 #endif
