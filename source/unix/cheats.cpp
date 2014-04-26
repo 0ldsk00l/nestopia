d83d4e4725de335c0352d87345f124c6bd008e2a
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2014-04-26 14:49:45 -0400

    Initial cheat support

--- source/unix/cheats.cpp
+++ source/unix/cheats.cpp
@@ -20,5 +20,70 @@
  * 
  */
 
+#include <iostream>
+#include <fstream>
+
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "main.h"
+#include "cheats.h"
+
+extern Emulator emulator;
+extern nstpaths_t nstpaths;
+
+typedef Nes::Core::Xml Xml;
+
+void cheats_init() {
+	// Initialize cheat engine
+	Cheats cheats(emulator);
+	Xml xml;
+	
+	cheats.ClearCodes();
+	
+	std::ifstream cheatfile(nstpaths.cheatpath, std::ifstream::in|std::ifstream::binary);
+	
+	if (cheatfile.is_open()) {
+		xml.Read(cheatfile);
+		
+		if (xml.GetRoot().IsType(L"cheats")) {
+			
+			Xml::Node root(xml.GetRoot());
+			Xml::Node node(xml.GetRoot().GetFirstChild());
+			
+			for (int i = 0; i < root.NumChildren(L"cheat"); i++) {
+				
+				if (node.GetAttribute(L"enabled").IsValue(L"1")) {
+					
+					if (node.GetChild(L"genie")) { // Game Genie
+						cheats_code_gg_add(node.GetChild(L"genie").GetValue());
+					}
+					
+					else if (node.GetChild(L"rocky")) { // Pro Action Rocky
+					}
+					
+					else if (node.GetChild(L"address")) { // Raw
+					}
+					
+					fprintf(stderr, "Cheat: %ls\n", node.GetChild(L"description").GetValue());
+				}
+				node = node.GetNextSibling();
+			}
+		}
+		cheatfile.close();
+	}
+	
+	//printf("Numcodes: %d\n", cheats.NumCodes());
+}
+
+void cheats_code_gg_add(const wchar_t *data) {
+	// Add a Game Genie code
+	Cheats cheats(emulator);
+	Cheats::Code code;
+	
+	char gg[9];
+	wcstombs(gg, data, sizeof(gg));
+	
+	cheats.GameGenieDecode(gg, code);
+	cheats.SetCode(code);
+}
