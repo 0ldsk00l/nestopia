2051fb3394ecaee6f47bfc9b6df1f25a2ba9306d
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2014-06-21 16:29:40 -0400

    Added application icon

--- source/unix/gtkui/gtkui.h
+++ source/unix/gtkui/gtkui.h
@@ -9,7 +9,7 @@ void gtkui_toggle_fullscreen();
 void gtkui_resize();
 void gtkui_set_title(const char *title);
 GtkWidget *gtkui_about();
-
+void gtkui_image_paths();
 void gtkui_opengl_start();
 
 void gtkui_swapbuffers();
