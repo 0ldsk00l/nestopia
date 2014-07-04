91d35414b8c4bc4c24a67804ced92e34ede4626a
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2014-07-03 22:21:46 -0400

    Drag and Drop works

--- source/unix/gtkui/gtkui_callbacks.h
+++ source/unix/gtkui/gtkui_callbacks.h
@@ -41,4 +41,6 @@ unsigned int gtkui_cb_translate_gdk_sdl(int gdk_keyval);
 int gtkui_cb_convert_key(GtkWidget *grab, GdkEventKey *event, gpointer userdata);
 int gtkui_cb_convert_mouse(GtkDrawingArea *area, GdkEventButton *event, gpointer userdata);
 
+void gtkui_drag_data(GtkWidget *widget, GdkDragContext *dragcontext, gint x, gint y, GtkSelectionData *seldata, guint info, guint time, gpointer data);
+
 #endif
