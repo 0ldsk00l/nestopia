0cb628418f9d93fa316a4e41d1e2908e8d7fea36
Ryan Danbrook <rdanbrook98@gmail.com>
2014-08-04 23:31:21 -0400

    Rewrote SDL audio

--- source/unix/audio.h
+++ source/unix/audio.h
@@ -14,11 +14,6 @@ void audio_pause();
 void audio_unpause();
 void audio_set_params(Sound::Output *soundoutput);
 void audio_adj_volume();
-void audio_sdl_callback(void *userdata, Uint8 *stream, int len);
-void audio_ao_callback(char *stream, int len);
-void audio_set_samples(uint32_t samples_per_frame);
-void audio_fill_buffer(int bufnum);
-void audio_output_frame(unsigned long numsamples, int16_t *out);
 
 bool timing_frameskip();
 void timing_set_default();
