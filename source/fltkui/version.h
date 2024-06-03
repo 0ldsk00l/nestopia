a5e85c480e2b50e77461fb20d5669a5cead3b517
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2024-06-03 09:11:39 -0600

    FLTK: Actually add version.h to source control

---
+++ source/fltkui/version.h
@@ -0,0 +1,13 @@
+#if 0
+# this file is valid C, Makefile and shell
+# this is the only file required to update when changing the version
+
+# https://semver.org/
+VERSION_MAJOR=1
+VERSION_MINOR=52
+VERSION_PATCH=1
+#endif
+
+#ifndef JG_VERSION
+#define JG_VERSION "1.52.1"
+#endif
