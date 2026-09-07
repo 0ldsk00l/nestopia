21939036d9b9db2810401cf20c2097a0527ccfcd
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2026-09-07 10:32:25 -0600

    Remove FLTK port

--- source/fltkui/crc32.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-uint32_t crc32(uint32_t crc, const void *data, size_t size);
--- source/fltkui/fltkui_archive.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-void fltkui_archive_load_file(const char *filename, std::string& arcname, std::vector<uint8_t>& game);
-bool fltkui_archive_select(const char *filename, std::string& arcname);
